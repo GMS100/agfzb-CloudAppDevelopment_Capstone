1e612b64603bf2585e85f76c3b25adf6885db5fc
GMS100 <2021GSonda@gmail.com>
2022-06-23 18:37:52 +0000

    Make Admin completed

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, default=None)
+    desc = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +24,29 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'SE'
+    COUPE = 'CO'
+    SUV = 'SU'
+    PICKUP = 'PI'
+    WAGON = 'WA'
+    MINIVAN = 'MI'
 
+    BODY_TYPE_CHOICES = [(SEDAN, 'Sedan'),
+                         (COUPE, 'Coupe'),
+                         (SUV, 'Sport Utility Vehicle'),
+                         (PICKUP, 'Pickup Truck'),
+                         (WAGON, 'Station Wagon'),
+                         (MINIVAN, 'Minivan')]
+
+    make = models.ForeignKey(CarMake, default=0, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    dealerID = models.IntegerField()
+    year = models.DateField(default=now())
+    bodyType = models.CharField(max_length=2, choices=BODY_TYPE_CHOICES)
+
+    def __str__(self):
+        return (self.year.strftime('%Y') + " " + self.make.name + " " + self.name)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
