27c1f07f7bcc87b455f68549116dbd02c69f269d
GMS100 <2021gsonda@gmail.com>
2022-07-05 00:59:20 +0000

    Added initial view of dealers

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -42,13 +42,36 @@ class CarModel(models.Model):
     make = models.ForeignKey(CarMake, default=0, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     dealerID = models.IntegerField()
-    year = models.DateField(default=now())
+    year = models.DateField(default=now)
     bodyType = models.CharField(max_length=2, choices=BODY_TYPE_CHOICES)
 
     def __str__(self):
         return (self.year.strftime('%Y') + " " + self.make.name + " " + self.name)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     # path for home page
     path(route='', view=views.get_dealerships, name='index'),
     # path for dealer reviews view
-
+    path(route='', view=views.get_dealerships, name='index'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
